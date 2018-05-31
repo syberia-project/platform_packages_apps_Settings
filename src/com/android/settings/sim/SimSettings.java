dce6056e930556208ecec342ca84c73a12747e12
Michael W <baddaemon87@gmail.com>
2018-05-31 14:11:34 +0200

    SimSettings: Fix preferred calls sim not being disabled
    * When disabling a sim on a dual sim device, the selection
      would not be disabled properly
    
    Change-Id: Ib8054f724ae974640491d03c668066db47b8a3a9
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -245,7 +245,7 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
         simPref.setSummary(phoneAccount == null
                 ? mContext.getResources().getString(R.string.sim_calls_ask_first_prefs_title)
                 : (String)telecomManager.getPhoneAccount(phoneAccount).getLabel());
-        simPref.setEnabled(allPhoneAccounts.size() > 1);
+        simPref.setEnabled(allPhoneAccounts.size() > 1 && mSelectableSubInfos.size() > 1);
     }
 
     @Override
