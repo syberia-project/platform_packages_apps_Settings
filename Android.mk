42471e15ce10587f95d4c6c05b7036588eee444e
SKULSHADY <anushekprasal@gmail.com>
2018-11-14 10:13:49 +0300

    Cleanup Android.mk
    Change-Id: Ib05f38133b44be813d8765c13f0530ba5eaf2f51
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- Android.mk
+++ Android.mk
@@ -51,16 +51,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     glide \
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v7/recyclerview/res \
     packages/apps/SyberiaSettings/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.preference \
-    --extra-packages android.support.v14.preference \
-    --extra-packages android.support.v17.preference \
-    --extra-packages android.support.v7.recyclerview \
     --extra-packages com.syberia.settings
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
