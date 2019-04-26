omnirom/android_packages_apps_Settings

[2/2] Settings: show AOD on charge setting
Change-Id: Iff6d8fea692a06c394e36a1ae821f1623ba67e67

src/com/android/settings/display/AmbientDisplayPreferenceController.java
@@ -17,6 +17,7 @@ package com.android.settings.display;
 
 import android.content.Context;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
@@ -47,10 +48,14 @@ public class AmbientDisplayPreferenceController extends AbstractPreferenceContro
     @Override
     public void updateState(Preference preference) {
         super.updateState(preference);
-        if (mConfig.alwaysOnEnabled(MY_USER_ID)) {
+        final boolean dozeOnChargeEnabled = (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.OMNI_DOZE_ON_CHARGE, 0) != 0);
+        if (mConfig.alwaysOnEnabledSetting(MY_USER_ID)) {
             preference.setSummary(R.string.ambient_display_screen_summary_always_on);
         } else if (mConfig.pulseOnNotificationEnabled(MY_USER_ID)) {
             preference.setSummary(R.string.ambient_display_screen_summary_notifications);
+        } else if (dozeOnChargeEnabled) {
+            preference.setSummary(R.string.doze_on_charge_summary);
         } else if (mConfig.enabled(MY_USER_ID)) {
             preference.setSummary(R.string.switch_on_text);
         } else {

syberia-project/platform_packages_apps_Settings

Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Signed-off-by: mracar07 <umutcan@umutcanacar.me>

src/com/android/settings/display/AmbientDisplayPreferenceController.java
@@ -17,6 +17,7 @@ package com.android.settings.display;
 
 import android.content.Context;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
@@ -47,10 +48,14 @@ public class AmbientDisplayPreferenceController extends AbstractPreferenceContro
     @Override
     public void updateState(Preference preference) {
         super.updateState(preference);
-        if (mConfig.alwaysOnEnabled(MY_USER_ID)) {
+        final boolean dozeOnChargeEnabled = (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.DOZE_ON_CHARGE, 0) != 0);
+        if (mConfig.alwaysOnEnabledSetting(MY_USER_ID)) {
             preference.setSummary(R.string.ambient_display_screen_summary_always_on);
         } else if (mConfig.pulseOnNotificationEnabled(MY_USER_ID)) {
             preference.setSummary(R.string.ambient_display_screen_summary_notifications);
+        } else if (dozeOnChargeEnabled) {
+            preference.setSummary(R.string.doze_on_charge_summary);
         } else if (mConfig.enabled(MY_USER_ID)) {
             preference.setSummary(R.string.switch_on_text);
         } else {