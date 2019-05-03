23bcbf54bd089ae50f5648878da1da872b8b5753
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-03 14:18:10 -0300

    Settings: Changes for LiveDisplay
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_packages_apps_LineageParts
    
    Change-Id: Ibf12e0141947cbffa74b3c005d10efc11dfc591d
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -184,4 +184,8 @@ public class Settings extends SettingsActivity {
     public static class PulseSettingsActivity extends SettingsActivity {}
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
     public static class PieSettingsActivity extends SettingsActivity { /* empty */ }
+
+    // LiveDisplay
+    public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+
 }
