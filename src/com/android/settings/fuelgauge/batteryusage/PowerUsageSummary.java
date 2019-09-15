f1bb4dee4c8b97ea4b63acfa921c33bfd70ee371
DennySPB <dennyspb@gmail.com>
2019-09-15 04:17:29 +0200

    Settings: Show battery temp on power summary
    [neobuddy89:]
    * Change layout to match other preferences
    * Fixed decimal value of battery temperature
    
    xNombre: forward port to S
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I868d12992ad44b018237bf9804aeb8cfe2f2ba2a
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -65,6 +65,10 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @VisibleForTesting
     static final String KEY_BATTERY_USAGE = "battery_usage_summary";
 
+    private static final String KEY_BATTERY_TEMP = "battery_temperature";
+
+    @VisibleForTesting
+    PowerGaugePreference mBatteryTempPref;
     @VisibleForTesting
     PowerUsageFeatureProvider mPowerFeatureProvider;
     @VisibleForTesting
@@ -160,6 +164,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         initFeatureProvider();
         initPreference();
 
+        mBatteryTempPref = (PowerGaugePreference) findPreference(KEY_BATTERY_TEMP);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
         if (Utils.isBatteryPresent(getContext())) {
@@ -239,6 +244,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
         }
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
+
+        mBatteryTempPref.setSummary(BatteryInfo.batteryTemp / 10 + " °C");
     }
 
     @VisibleForTesting
