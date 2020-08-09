27376f22ff115ec0988a26e36c876b00c6596f75
Joey Huab <joey@evolution-x.org>
2020-08-09 06:43:19 +0000

    Screen off fod: Make it optional
    * Since not all FOD supported devices have the proper support for Screen off FOD, hide it by default for all.
    
    Change-Id: Id20b740f99e95b5dd051cd394e42cc2df887b10b
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -78,6 +78,7 @@ public class ScreenLockSettings extends DashboardFragment
         controllers.add(new QuickUnlockPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
+        controllers.add(new ScreenOffFodPreferenceController(context));
         return controllers;
     }
 
