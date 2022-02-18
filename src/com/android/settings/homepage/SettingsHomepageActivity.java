4dd878f70e34c307e0befde7a2a2b4c1f7559e7c
dlwlrma123 <alexfinhart@gmail.com>
2022-02-18 19:52:06 +0000

    Settings: Redesigned settings home page
    Co-authored-by: beingmishra <rahul731999@gmail.com>
    Co-authored-by: spkal01 <spkal01@users.noreply.github.com>
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ib8c02ad1598c192d4be8e0117daf6018005ff76d
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -23,6 +23,7 @@ import static android.provider.Settings.EXTRA_SETTINGS_EMBEDDED_DEEP_LINK_INTENT
 import static com.android.settings.SettingsActivity.EXTRA_USER_HANDLE;
 
 import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
 import android.app.ActivityManager;
 import android.app.settings.SettingsEnums;
 import android.content.ComponentName;
@@ -46,6 +47,8 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.annotation.VisibleForTesting;
@@ -64,7 +67,6 @@ import com.android.settings.R;
 import com.android.settings.Settings;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsApplication;
-import com.android.settings.accounts.AvatarViewMixin;
 import com.android.settings.activityembedding.ActivityEmbeddingRulesController;
 import com.android.settings.activityembedding.ActivityEmbeddingUtils;
 import com.android.settings.core.CategoryMixin;
@@ -74,7 +76,9 @@ import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.safetycenter.SafetyCenterManagerWrapper;
 import com.android.settingslib.Utils;
 import com.android.settingslib.core.lifecycle.HideNonSystemOverlayMixin;
-
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import java.util.ArrayList;
 import java.net.URISyntaxException;
 import java.util.Set;
 
@@ -102,8 +106,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
 
     private TopLevelSettings mMainFragment;
     private View mHomepageView;
-    private View mSuggestionView;
-    private View mTwoPaneSuggestionView;
     private CategoryMixin mCategoryMixin;
     private Set<HomepageLoadedListener> mLoadedListeners;
     private ActivityEmbeddingController mActivityEmbeddingController;
@@ -111,6 +113,75 @@ public class SettingsHomepageActivity extends FragmentActivity implements
     private boolean mIsTwoPane;
     // A regular layout shows icons on homepage, whereas a simplified layout doesn't.
     private boolean mIsRegularLayout = true;
+
+    CollapsingToolbarLayout collapsing_toolbar;
+    
+    static ArrayList<String> text=new ArrayList<>();
+    static {
+        text.add("Arise, Young one.");
+	    text.add("Welcome Stranger!");
+	    text.add("One's soul shreds uniqueness.");
+        text.add("Calm down young one, catch your breath.");
+        text.add("Roquelaire, would you like a cracker?");
+        text.add("My little friend, always busy-busy.");
+        text.add("Ho ho! You found me!");
+        text.add("Welcome to the Secret Shop!");
+        text.add("Some tea while you wait?");
+        text.add("Those go together nicely.");
+        text.add("Your foes will fear you now.");
+        text.add("My favorite customer!");
+        text.add("Business is brisk.");
+        text.add("How is your journey little one?");
+        text.add("Mistakes are always part of one's life, youngster.");
+        text.add("Have a lucky day human!");
+        text.add("You can do it Stranger!");
+	    text.add("It was never wrong to try, young one.");
+	    text.add("The learned one strikes.");
+	    text.add("They will never know what hit them.");
+	    text.add("Turn the tables!");
+	    text.add("The enemy will be destroyed, no matter the cost!");
+	    text.add("A good strategist always keeps something in reserve.");
+	    text.add("Never Settle?");
+	    text.add("Gratitude unlocks the fullness of life, Milord.");
+	    text.add("A joker is a little fool who is different from everyone else.");
+	    text.add("Failure is not Fatal, Customer.");
+	    text.add("Taking a rest is not a sin young man.");
+	    text.add("What is truth, but a survivor's story?");
+	    text.add("In a world without love, death means nothing.");
+	    text.add("Always appreciate your own endeavors, Milord.");
+	    text.add("Fear is the first of many foes.");
+	    text.add("The climb may be long, but the view is worth it.");
+	    text.add("The waves will drag you down, unless you fight to shore.");
+	    text.add("The darker the night, the brighter the stars.");
+	    text.add("Fight and be remembered, or die and be forgotten.");
+	    text.add("In case no one asked, are you doing fine youngster?");
+	    text.add("Nothing bears fruit from hatred, but disaster my friend.");
+	    text.add("Another day to become a legend.");
+	    text.add("In case no one told you this, you are awesome!");
+	    text.add("My dear friend always busy, want some cookies?");
+	    text.add("Never Forget the Arcanery.");
+	    text.add("Show em what you got stranger!");
+	    text.add("The Arcanery likes your presence.");
+	    text.add("What shall the Arcanery grant unto you?");
+	    text.add("Life is always full of mysteries.");
+	    text.add("Find what you seek on Tresdins Lair.");
+	    text.add("Seek and you shall find.");
+	    text.add("Everyone is a survivor from the cruel reality.");
+	    text.add("The Arcanery loves your efforts.");
+	    text.add("Destiny awaits us all.");
+	    text.add("From knowledge comes skill.");
+	    text.add("What must be discovered?");
+	    text.add("Even a master falters.");
+    }
+    
+    static ArrayList<String> welcome=new ArrayList<>();
+    static {
+        welcome.add("Hi!");
+        welcome.add("Hello.");
+        welcome.add("Greetings.");
+        welcome.add("Good Day!");
+        welcome.add("Settings");
+    }
 
     /** A listener receiving homepage loaded events. */
     public interface HomepageLoadedListener {
@@ -142,25 +213,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         }
     }
 
-    /**
-     * Shows the homepage and shows/hides the suggestion together. Only allows to be executed once
-     * to avoid the flicker caused by the suggestion suddenly appearing/disappearing.
-     */
-    public void showHomepageWithSuggestion(boolean showSuggestion) {
-        if (mHomepageView == null) {
-            return;
-        }
-        Log.i(TAG, "showHomepageWithSuggestion: " + showSuggestion);
-        final View homepageView = mHomepageView;
-        mSuggestionView.setVisibility(showSuggestion ? View.VISIBLE : View.GONE);
-        mTwoPaneSuggestionView.setVisibility(showSuggestion ? View.VISIBLE : View.GONE);
-        mHomepageView = null;
-
-        mLoadedListeners.forEach(listener -> listener.onHomepageLoaded());
-        mLoadedListeners.clear();
-        homepageView.setVisibility(View.VISIBLE);
-    }
-
     /** Returns the main content fragment */
     public TopLevelSettings getMainFragment() {
         return mMainFragment;
@@ -174,7 +226,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         mIsEmbeddingActivityEnabled = ActivityEmbeddingUtils.isEmbeddingActivityEnabled(this);
         if (mIsEmbeddingActivityEnabled) {
             final UserManager um = getSystemService(UserManager.class);
@@ -202,30 +253,37 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         mIsTwoPane = mActivityEmbeddingController.isActivityEmbedded(this);
 
         updateAppBarMinHeight();
-        initHomepageContainer();
-        updateHomepageAppBar();
         updateHomepageBackground();
         mLoadedListeners = new ArraySet<>();
 
-        initSearchBarView();
+        final View root = findViewById(R.id.settings_homepage_container);
+	LinearLayout commonCon = root.findViewById(R.id.common_con);
+        final Toolbar toolbar = root.findViewById(R.id.search_action_bar);
+	collapsing_toolbar =  root.findViewById(R.id.collapsing_toolbar);
+	TextView greeter = root.findViewById(R.id.greeter);
+	greeter.setText(text.get(randomNum(0, text.size()-1)));
+
+        FeatureFactory.getFactory(this).getSearchFeatureProvider()
+                .initSearchToolbar(this /* activity */, toolbar, SettingsEnums.SETTINGS_HOMEPAGE);
+                
+        AppBarLayout appBarLayout = root.findViewById(R.id.appbar);
+        appBarLayout.addOnOffsetChangedListener((appBarLayout1, i) -> {
+        	
+        	float abs = ((float) Math.abs(i)) / ((float) appBarLayout1.getTotalScrollRange());
+            float f2 = 1.0f - abs;
+            //greeter text
+            if (f2 == 1.0)
+                ObjectAnimator.ofFloat(greeter, View.ALPHA, 1f).setDuration(500).start();
+            else
+                greeter.setAlpha(0f);
+        });
 
         getLifecycle().addObserver(new HideNonSystemOverlayMixin(this));
+        collapsing_toolbar.setTitle(welcome.get(randomNum(0, welcome.size()-1)));
         mCategoryMixin = new CategoryMixin(this);
         getLifecycle().addObserver(mCategoryMixin);
 
-        final String highlightMenuKey = getHighlightMenuKey();
-        // Only allow features on high ram devices.
-        if (!getSystemService(ActivityManager.class).isLowRamDevice()) {
-            initAvatarView();
-            final boolean scrollNeeded = mIsEmbeddingActivityEnabled
-                    && !TextUtils.equals(getString(DEFAULT_HIGHLIGHT_MENU_KEY), highlightMenuKey);
-            showSuggestionFragment(scrollNeeded);
-            if (FeatureFlagUtils.isEnabled(this, FeatureFlags.CONTEXTUAL_HOME)) {
-                showFragment(() -> new ContextualCardsFragment(), R.id.contextual_cards_content);
-                ((FrameLayout) findViewById(R.id.main_content))
-                        .getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
-            }
-        }
+	final String highlightMenuKey = getHighlightMenuKey();
         mMainFragment = showFragment(() -> {
             final TopLevelSettings fragment = new TopLevelSettings();
             fragment.getArguments().putString(SettingsActivity.EXTRA_FRAGMENT_ARG_KEY,
@@ -288,7 +346,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         if (!mIsEmbeddingActivityEnabled) {
             return;
         }
-
         if (mIsTwoPane) {
             if (mIsRegularLayout == ActivityEmbeddingUtils.isRegularHomepageLayout(this)) {
                 // Layout unchanged
@@ -344,18 +401,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         }
     }
 
-    private void initAvatarView() {
-        final ImageView avatarView = findViewById(R.id.account_avatar);
-        final ImageView avatarTwoPaneView = findViewById(R.id.account_avatar_two_pane_version);
-        if (AvatarViewMixin.isAvatarSupported(this)) {
-            avatarView.setVisibility(View.VISIBLE);
-            getLifecycle().addObserver(new AvatarViewMixin(this, avatarView));
-
-            if (mIsEmbeddingActivityEnabled) {
-                avatarTwoPaneView.setVisibility(View.VISIBLE);
-                getLifecycle().addObserver(new AvatarViewMixin(this, avatarTwoPaneView));
-            }
-        }
     }
 
     private void updateHomepageBackground() {
@@ -373,30 +418,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         window.setStatusBarColor(color);
         // Update content background.
         findViewById(android.R.id.content).setBackgroundColor(color);
-    }
-
-    private void showSuggestionFragment(boolean scrollNeeded) {
-        final Class<? extends Fragment> fragmentClass = FeatureFactory.getFactory(this)
-                .getSuggestionFeatureProvider(this).getContextualSuggestionFragment();
-        if (fragmentClass == null) {
-            return;
-        }
-
-        mSuggestionView = findViewById(R.id.suggestion_content);
-        mTwoPaneSuggestionView = findViewById(R.id.two_pane_suggestion_content);
-        mHomepageView = findViewById(R.id.settings_homepage_container);
-        // Hide the homepage for preparing the suggestion. If scrolling is needed, the list views
-        // should be initialized in the invisible homepage view to prevent a scroll flicker.
-        mHomepageView.setVisibility(scrollNeeded ? View.INVISIBLE : View.GONE);
-        // Schedule a timer to show the homepage and hide the suggestion on timeout.
-        mHomepageView.postDelayed(() -> showHomepageWithSuggestion(false),
-                HOMEPAGE_LOADING_TIMEOUT_MS);
-        showFragment(new SuggestionFragCreator(fragmentClass, /* isTwoPaneLayout= */ false),
-                R.id.suggestion_content);
-        if (mIsEmbeddingActivityEnabled) {
-            showFragment(new SuggestionFragCreator(fragmentClass, /* isTwoPaneLayout= */ true),
-                    R.id.two_pane_suggestion_content);
-        }
     }
 
     private <T extends Fragment> T showFragment(FragmentCreator<T> fragmentCreator, int id) {
@@ -718,5 +739,10 @@ public class SettingsHomepageActivity extends FragmentActivity implements
                 ((SplitLayoutListener) fragment).setSplitLayoutSupported(mIsTwoPaneLayout);
             }
         }
+    }
+
+    private int randomNum(int min , int max) {
+	int r = (max - min) + 1;
+	return (int)(Math.random() * r) + min;
     }
 }
