beababb21c568b51ab983cdf75132a38e21e0753
DennySPb <dennyspb@gmail.com>
2022-06-22 16:28:31 +0300

    Adapt new designed look to syberia
        * remove useless greeter (it takes a 30% of screen)
        * add back changelog and radio info preference
        * use or vector logo for device info screen
        * fix build in A13
    Change-Id: I378e7ab2d5d24b5a5d78c4e18a50916c4ac9436c
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -208,7 +208,6 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
    	 onSetPrefCard();
 
         }
-    }
 
 	private void onSetPrefCard() {
 	final PreferenceScreen screen = getPreferenceScreen();
